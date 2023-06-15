d3e02cf8c180da5f2e2742a23f363a4601926f91
omusubitabetai <ec2-user@ip-172-31-1-90.ap-northeast-1.compute.internal>
2023-06-15 15:36:34 +0000

    Chapter3-9から3-11まで

--- ColorPicker/js/script.js
+++ ColorPicker/js/script.js
@@ -2,5 +2,17 @@
 //document.querySelector('#colorText').textContent = `カラーコード:${document.querySelector('#colorPicker').value}`;
 const text = document.querySelector('#colorText');
 const color = document.querySelector('#colorPicker');
-text.textContent = `カラーコード:${color.value}`;
+
+const colorBg = () => {
+  document.body.style.backgroundColor = color.value;
+
+  if (color.value === '#ffffff') {
+    text.textContent = `カラーコード: ${color.value} (white)`;
+  } else if (color.value === '#000000') {
+    text.textContent = `カラーコード: ${color.value} (black)`;
+  } else {
+    text.textContent = `カラーコード: ${color.value}`;
+  }
+}
+
 color.addEventListener('input', colorBg);
