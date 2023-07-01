650b36fb6d326e3166b3af46c2b1e8a1d245cac6
omusubitabetai <ec2-user@ip-172-31-1-90.ap-northeast-1.compute.internal>
2023-07-01 09:01:19 +0000

    Chapter６-7から6−12まで

---
+++ chapter6/09-demo/js/script.js
@@ -0,0 +1,18 @@
+const items = document.querySelectorAll('.img-item');
+//console.log(items);
+
+for (let i = 0; i < items.length; i++) {
+  const keyframes = {
+    filter: ['blur(20px)','blur(0)'],
+    //opacity: [0, 1],
+    //rotate: ['x 5deg', 0],
+    //scale: [1.1, 1],
+    //translate: ['o 50px', 0],
+  };
+  const options = {
+    duration: 600,
+    delay: i * 300,
+    fill: 'forwards',
+  };
+  items[i].animate(keyframes, options);
+}
--- chapter6/js/script.js
+++ chapter6/js/script.js
@@ -1,8 +1,18 @@
 const heading = document.querySelector('#heading');
 
-const keyframes ={
-  opacity: [0,1],
-  translate: ['0 50px',0],
-}
 
-heading.animate(keyframes, 2000);
+const keyframes = {
+  borderRadius: [
+      '20% 50% 50% 70%/50% 50% 70% 50%',
+      '50% 20% 50% 50%/40% 40% 60% 60%',
+      '50% 40% 20% 40%/40% 50% 50% 80%',
+      '50% 50% 50% 20%/40% 40% 60% 60%',
+    ],
+};
+const options = {
+  duration: 8000,
+  direction: 'alternate',
+  iterations: Infinity,
+};
+
+heading.animate(keyframes, options);
